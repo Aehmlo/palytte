9ed3b8792e11e19c5c06a65085008fce232ac6a4
Alex Hamilton <1622250+Aehmlo@users.noreply.github.com>
2025-03-10 09:52:30 -0400

    Remove flake-utils dependency
    This revision removes the previous flake-utils dependency in favor of
    hand-rolled wrappers around nixpkgs.lib.genAttrs. To retain the ability
    to override build systems, the flake-utils systems input has been moved
    to be a direct flake input.

--- flake.nix
+++ flake.nix
@@ -1,23 +1,36 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    systems.url = "github:nix-systems/default";
   };
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, systems }:
     let
-      pkgs = nixpkgs.legacyPackages.${system};
-      inherit (pkgs) lib;
-      haskellPackages = pkgs.haskellPackages.extend (pkgs.haskell.lib.compose.packageSourceOverrides {
-        palytte = self;
-      });
+      forAllSystems = nixpkgs.lib.genAttrs (import systems);
+      forAllPkgs = f: forAllSystems (system: f nixpkgs.legacyPackages.${system});
+      forAllHaskellPackages' = f: forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          inherit (pkgs) lib;
+          haskellPackages = pkgs.haskellPackages.extend (pkgs.haskell.lib.compose.packageSourceOverrides {
+            palytte = self;
+          });
+        in
+        f pkgs haskellPackages
+      );
+      forAllHaskellPackages = f: forAllHaskellPackages' (pkgs: haskellPackages: f haskellPackages);
     in
     {
-      devShells.default = haskellPackages.shellFor {
-        packages = ps: [ ps.palytte ];
-        withHoogle = true;
-        buildInputs = with pkgs; [ hlint just ormolu ];
-      };
-      packages.default = haskellPackages.palytte;
-      formatter = pkgs.nixpkgs-fmt;
-    });
+      devShells = forAllHaskellPackages' (pkgs: haskellPackages: {
+        default = haskellPackages.shellFor {
+          packages = ps: [ ps.palytte ];
+          withHoogle = true;
+          buildInputs = with pkgs; [ hlint just ormolu ];
+        };
+      });
+      packages = forAllHaskellPackages (hs: {
+        inherit (hs) palytte;
+        default = hs.palytte;
+      });
+      formatter = forAllPkgs (pkgs: pkgs.nixpkgs-fmt);
+    };
 }
